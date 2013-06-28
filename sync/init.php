c02476bbb58d47400fa703a3d6fd58dace72dea6
SprayHank <fantasyoui@gmail.com>
2013-06-28 12:53:56 +0800

    路径修正

--- sync/init.php
+++ sync/init.php
@@ -4,7 +4,7 @@ define('DIR', dirname(__FILE__));
 
 spl_autoload_register('sync_autoload');
 function sync_autoload($class) {
-	$cls = './class/'.$class.'.class.php';
+	$cls = DIR.'/class/'.$class.'.class.php';
 	is_file($cls) && is_readable($cls) && require($cls);//目标为文件（非目录），可读，载入
 }
 
